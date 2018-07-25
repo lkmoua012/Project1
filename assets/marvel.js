45eb7b5018eca9cd63ec34b66f14894886bc6b6b
Leonidas <theRealNeotheone@users.noreply.github.com>
2018-07-24 20:15:54 -0700

    finally

--- assets/marvel.js
+++ assets/marvel.js
@@ -1,6 +1,35 @@
 var activeName = "";
 
+function marvelAPI() {
+  
+    var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name="+ activeName + "&ts=1&apikey=3729b58ef60ba8affe97db66144af1c2&hash=8a8767c9ba9a5c15ae56cd72bc2718e9";
+     
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+  
+        var portrait = (response.data.results[0].thumbnail.path + "." + response.data.results[0].thumbnail.extension);
+  
+        $(".portrait").attr("src", portrait);
+        $(".charName").text(response.data.results[0].name);
+       $(".description").text(response.data.results[0].description);
+  
+      //  console.log(response);
+      //  console.log(portrait);
+  
+      });
+     //^ End of AJAX
+  };
+
 $(document).ready(function() {
+
+  $(".char").click(function() {
+     
+       activeName = $(this).text();
+       marvelAPI();
+    
+      });
 
     
 
